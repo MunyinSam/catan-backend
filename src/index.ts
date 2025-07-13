0ee38ca1dc61b9df6cbc16256996aaa13d0d6aaf
MunyinSam <kelvinsam233@gmail.com>
2025-07-13 17:27:05 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+// server.ts
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+})
+
+type Room = {
+    players: string[]
+}
+
+const rooms: Record<string, Room> = {}
+
+function generateRoomCode(length = 5) {
+    return Math.random()
+        .toString(36)
+        .substring(2, 2 + length)
+        .toUpperCase()
+}
+
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id)
+
+    socket.on('createGame', () => {
+        const roomCode = generateRoomCode()
+        rooms[roomCode] = { players: [socket.id] }
+        socket.join(roomCode)
+        socket.emit('gameCreated', roomCode)
+        io.to(roomCode).emit('playerList', rooms[roomCode].players)
+        console.log('Game created:', roomCode)
+    })
+
+    socket.on('joinGame', (roomCode: string) => {
+        const room = rooms[roomCode]
+        if (room && room.players.length < 6) {
+            room.players.push(socket.id)
+            socket.join(roomCode)
+
+            socket.emit('joinedGame', roomCode)
+            socket.emit('playerList', room.players)
+
+            socket.to(roomCode).emit('playerList', room.players)
+        } else {
+            socket.emit('error', 'Room not found or full.')
+        }
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id)
+
+        for (const [roomCode, room] of Object.entries(rooms)) {
+            const index = room.players.indexOf(socket.id)
+            if (index !== -1) {
+                room.players.splice(index, 1)
+                io.to(roomCode).emit('playerList', room.players)
+            }
+
+            if (room.players.length === 0) {
+                delete rooms[roomCode]
+            }
+        }
+    })
+})
+
+httpServer.listen(4000, () => {
+    console.log('Socket server running on http://localhost:4000')
+})
